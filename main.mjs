4fedda788e6683061a479d3eeb635ee870bbfb53
Axel669 <haryuu.hanekata@gmail.com>
2022-08-28 21:20:21 -0700

    maybe this will even work on render?

---
+++ main.mjs
@@ -0,0 +1,53 @@
+import http from "http"
+
+import express from "express"
+import cors from "cors"
+import { Server } from "socket.io"
+
+const app = express()
+
+app.use(cors())
+app.use(
+    express.json({
+        limit: "10kb",
+    })
+)
+app.get(
+    "/:roomID",
+    (req, res, next) => {
+        res.json(
+            io.of(req.params.roomID).sockets.size
+        )
+    }
+)
+app.post(
+    "/broadcast/:roomID",
+    (req, res, next) => {
+        io.of(req.params.roomID).emit(
+            req.body.type,
+            req.body.data
+        )
+        res.json(true)
+    }
+)
+
+const nodeServer = http.createServer(app)
+const io = new Server(nodeServer)
+
+io.of("test").use(
+    (socket, next) => {
+        console.log(socket.handshake.headers.origin)
+        next()
+    }
+)
+io.of("test").on(
+    "connection",
+    (socket) => {
+        socket.emit("hi", [1, 2, 3, 4])
+    }
+)
+
+nodeServer.listen(
+    process.env.port ?? 4746,
+    () => console.log("Unus Mundus Network online")
+)
